f46022cf9ba066862814e08f89b98b2ec51d1872
son3102k <prokieumoi1@gmail.com>
2022-07-22 15:31:13 +0700

    login api

--- src/page/Login.js
+++ src/page/Login.js
@@ -15,9 +15,10 @@ import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
 
-import {useEffect} from "react";
-import {createTheme} from "@mui/material";
-
+import {useEffect, useState} from "react";
+import {Alert, createTheme, Snackbar} from "@mui/material";
+import axios from "axios";
+import {useNavigate} from "react-router";
 function Copyright(props) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
@@ -48,17 +49,40 @@ const font = createTheme({
 });
 
 export default function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [notify, setNotify] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
     useEffect(()=>{
         document.title = 'Đăng nhập'
     });
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
+    const handleCloseNotify = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setNotify(false);
+    };
+
+    const handleSubmit = async () => {
+        const requestBody = {
+            email,
+            password,
+        };
+        const response = await axios.post('http://localhost:8080/login',requestBody, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
         });
+        if (response.data['success']) {
+            navigate('createclientv3');
+        }
+        else {
+            setNotify(true);
+            setPassword('');
+        }
     };
 
     return (
@@ -93,7 +117,7 @@ export default function Login() {
                         <Typography component="h1" variant="h5" fontWeight="800" fontFamily={font.typography.fontFamily}>
                             Chào mừng Quý khách
                         </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, p: 1 }} >
+                    <Box noValidate sx={{ mt: 1, p: 1 }} >
                         <Box sx={{display: "flex", alignItems: "flex-end"}}>
                             <PersonOutlineIcon sx={{ color: '#33ccff', mr: 1, my: 0.5 }}/>
                             <TextField
@@ -107,6 +131,7 @@ export default function Login() {
                                 autoFocus
                                 variant="standard"
                                 sx={{minWidth: 350}}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                         </Box>
                         <Box sx={{display: "flex", alignItems: "flex-end"}}>
@@ -122,6 +147,8 @@ export default function Login() {
                                 autoComplete="current-password"
                                 variant="standard"
                                 sx={{minWidth: 350}}
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
                             />
                         </Box>
                         {/*<FormControlLabel sx={{mt: 2 }}*/}
@@ -129,11 +156,12 @@ export default function Login() {
                         {/*    label="Lưu thông tin"*/}
                         {/*/>*/}
                         <Button
-                            type="submit"
+                            // type="submit"
                             fullWidth
                             variant="contained"
                             sx={{ mt: 3, mb: 2 , fontSize: 13, p: 1.3 ,fontWeight: 'bold',
                                 backgroundImage: "linear-gradient(120deg,#00bfae 0,#0066ad 100%)"}}
+                            onClick={handleSubmit}
                         >
                             Đăng nhập
                         </Button>
@@ -154,6 +182,11 @@ export default function Login() {
 
             </Container>
         <Copyright sx={{width: '100%', color: '#ffffff', textAlign: 'right', mb: 2}}></Copyright>
+        <Snackbar open={notify} autoHideDuration={3000} onClose={handleCloseNotify}>
+            <Alert onClose={handleCloseNotify} severity="error" sx={{ width: '100%' }}>
+                Tài khoản hoặc mật khẩu không chính xác
+            </Alert>
+        </Snackbar>
     </Container>
     );
 }
